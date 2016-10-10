dc1d25b67dbf44aedb8b2d706f07a589da40ca21
wenqingyu <yuwenqingisu@gmail.com>
2016-10-10 13:40:40 +0800

    clear config

--- config.js
+++ config.js
@@ -11,36 +11,9 @@ module.exports = {
     }
   },
 
-  // USED BY PAYMENT GATEWAY CENTER SERVER
-  depositWithdraw: {
-      api: 'http://172.20.3.213:8734/secure/RPC',
-      signSecret: 'Btc_chin@_Future_exg_2015',
-
-      operatorAccount: '88',
-      operatorAccountKey: '18e803b2-202f-4e7e-94f7-06b0358ad88f'
-  },
-
-  // Adaptor's config
-  simplex: {
-    api: 'https://sandbox.test-simplexcc.com/v2/',
-    // BTCC Test Key
-    // api: 'https://api.Simplex.com/v2/',
-    apiKey: 'ApiKey eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwYXJ0bmVyIjoiYnRjYyIsInNhbmRib3giOnRydWV9.AnyTk5gwdvAaoDGLhFHzPX2Vyf3OoAzXNwHk-kUKHKE',
-    // sandbox Key
-    // apiKey: 'ApiKey eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwYXJ0bmVyIjoiZXhhbXBsZSIsInNhbmRib3giOnRydWV9.n6kOT1ATi7AfCLhhHHRew2zuEbvIU_64U17jrjVF1p0',
-    partnerName: 'BTCC'
-
-  },
-
-  kyc: {
-    api: 'https://staging.btcc.com/api.php/kyc',
-    secret: '$1$v1Ry8P3A$AWMb69kS7U8KQz3QUOM00'
-  },
-
   affiliate: {
-    api: 'localhost:3010',
+    api: 'localhost:3010', // affiliate api
     gateway: 'affiliate'
   }
-
 
 }
